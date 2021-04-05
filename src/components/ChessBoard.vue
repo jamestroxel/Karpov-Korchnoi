8c49e09741ec930e3422feca070aa488b68b83f1
jamestroxel <james.r.troxel@gmail.com>
2021-04-04 20:16:58 -0400

    sankeys

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -3,11 +3,13 @@
   <div id="board" style="width: 400px"></div>
   <div class="block" style="width: 400px">
   <button 
+  @click="nextMove"
   id="next"
   type="button">
   &#62;
   </button>
   <button 
+  @click="previousMove"
   id="previous"
   type="button">
   &#60;
@@ -23,53 +25,62 @@
 <script>
 import ChessBoard from "chessboardjs-vue";
 import moves from "./moves.json";
-import $ from 'jquery'
-// var board = Chessboard('board')
+
 export default {
   name: 'ChessBoard',
-  // components: {
-  //     ChessBoard
-  // },
+
    data(){
     return {
       moves: moves,
       moveIndex: 0
-      // position: null,
     }
   },
   updated(){
-       ChessBoard('board',"start").move();
+       ChessBoard('board',"start").move(this.getMove());
   },
   mounted(){
     this.nextMove();
     this.previousMove();
     this.moves = moves.move;
-       ChessBoard('board',"start");
+    // this.moveIndex = moves[this.moveIndex].move;
+    // ChessBoard('board',"start");
   },
   computed: {
     getMove() {
-      return this.moves[this.moveIndex]
-    },
-    getPosition(){
-      return ChessBoard('board').fen()
+      return this.moves[this.moveIndex].move
     }
   },
   methods: {
     // getPosition(){
     //   return ChessBoard('board').fen()
     // },
-    previousMove(){
-      $('#previous').on('click', function () { 
-        const pMove = this.getMove - 1;
-        return ChessBoard('board',moves[0].fen).move(pMove)
-      })
+    getPosition(){
+      return ChessBoard('board').fen()
     },
     nextMove(){
-      $('#next').on('click', function () {
-        const nMove = this.getMove + 1;
-        return ChessBoard('board',moves[1].fen).move(nMove)
-      })
-    }
+      return this.moveIndex + 1
+    },
+    previousMove(){
+      return this.moveIndex - 1
+    },
+    // nextMove(){
+    //   $('#next').on('click', function () {
+    //     return ChessBoard('board',moves[1].fen).move(nMove)
+    //   })
+    // }
+    // previousMove(){
+    //   $('#previous').on('click', function () { 
+    //     const pMove = this.getMove - 1;
+    //     return ChessBoard('board',moves[0].fen).move(pMove)
+    //   })
+    // },
+    // nextMove(){
+    //   $('#next').on('click', function () {
+    //     const nMove = this.getMove + 1;
+    //     return ChessBoard('board',moves[1].fen).move(nMove)
+    //   })
+    // }
   }
+  
 }
 </script>
