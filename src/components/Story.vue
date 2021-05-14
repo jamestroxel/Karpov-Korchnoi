a96299227105d0e605ab5cc05e6788683fb35b1c
jamestroxel <james.r.troxel@gmail.com>
2021-05-14 09:04:52 -0400

    housekeeping

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -13,19 +13,6 @@
         {{ copy[this.moveIndex].text }}
     </p>
 </div>
-    <!-- <el-carousel indicators=outside :autoplay="false" height="465px">
-        <el-carousel-item v-for="text in copy" :key="text.key">
-            <p :class="text.headingStyle">
-                {{text[this.moveIndex].heading}}
-            </p>
-             <p :class="text.subHeadingStyle">
-                {{text[this.moveIndex].subHeading}}
-            </p>
-            <p :class="text.style">
-                {{ text[this.moveIndex].text }}
-            </p>
-        </el-carousel-item>
-    </el-carousel> -->
 </template>
 <script>
     import story from '../assets/story.json';
